2f61b85ede38d24fdf103629be91df7ca2869625
kwoosh <tobirawork@gmail.com>
2017-07-20 17:50:07 +0300

    added stylus and pug, also vue-awesome

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
-    <img src="./assets/sounds/logo.png">
-    <hello></hello>
+    <header>
+
+    </header>
+
+    <main>
+      
+    </main>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
 
 export default {
   name: 'app',
-  components: {
-    Hello
-  }
+  components: {}
 }
+
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
