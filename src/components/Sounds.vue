a74ba58d5e58c8170478920ac57a8265cd84f074
kwoosh <tobirawork@gmail.com>
2017-07-24 16:44:27 +0300

    empty sounds

--- src/components/Sounds.vue
+++ src/components/Sounds.vue
@@ -1,49 +1,12 @@
 <template>
-  <ul>
-     <li v-for="(item, i) in music" :key="i" > 
-       <!-- {{ item.data.metadata.track }} - {{ item.data.metadata.title }}   -->
-      <audio ref="audio" :src="'file:// '+ item.data.file"></audio>
-      <button @click="playFile(item.filePath, i)">Play</button>
-      <button @click="stopFile(item.filePath, i)">Pause</button>
-    </li>
-  </ul>
+  
 </template>
 <script>
-import Noise from './Noise'
 
 export default {
   name: 'sounds',
-  methods: {
-    playFile(file, index) {
-      const music = this.$refs.audio[index];
-      if (music.paused) {
-        music.play();
-        console.log("play")
-      } else {
-        music.pause();
-        console.log(music.currentTime);
-      }
-    },
-    stopFile(file, index) {
-      const music = this.$refs.audio[index];
-      music.pause();
-      music.currentTime = 0;
-    }
-  },
-  data() {
-    return {
-      music: [
-        {
-          filePath: './assets/sounds/fireplace.mp3',
-          data: {
-            title: 'fireplace',
-            file: './assets/sounds/fireplace.mp3'
-          }
-        }
-      ]
-    }
-  }
 }
+
 </script>
 <style lang="stylus" scoped>
 
