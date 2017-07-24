1c12af6ffce406b4fca4b3980d2028f3e3cead1b
kwoosh <tobirawork@gmail.com>
2017-07-24 17:08:06 +0300

    works... finaly

--- src/components/Sounds.vue
+++ src/components/Sounds.vue
@@ -1,11 +1,36 @@
-<template>
-  
+<template lang="pug">
+
+  div(  ) 
+    noise( 
+      v-for="(item, i) in items" 
+      :key="i" 
+      :file="item.src" 
+      :icon="item.icon"
+      :working="item.isNoise" )
+
 </template>
 <script>
+import Noise from './Noise'
 
 export default {
   name: 'sounds',
+  components: { Noise },
+  data() {
+    return {
+      items: [
+        {
+          isNoise: false,
+          title: 'rain',
+          src: '../../static/sounds/rain.mp3',
+          icon: 'train'
+        }
+      ]
+    }
+  } 
 }
+
+
+
 
 </script>
 <style lang="stylus" scoped>
